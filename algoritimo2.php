2bc5e9139b1b936592a58489aa1d5cf071c6a1d2
Lucas  Teles <lucas.mkt@alterdata.com.br>
2021-05-30 13:44:28 -0300

    Algoritimo2 funcionando

--- algoritimo2.php
+++ algoritimo2.php
@@ -5,11 +5,6 @@ if (!defined("STDIN")) {
 
 echo "Digite insira o array a ser ordenado:\n";
 $input_array = trim(fgets(STDIN, 1024));
-// echo "Digite o numero de rotações:\n";
-// $input_times = fgets(STDIN, 1);
-// readline();
-
-// echo $input_times;
 $array = str_split($input_array);
 $aux = $array[0];
 $arrayPar = [];
@@ -18,7 +13,7 @@ $arrayImpar = [];
 function ehPar($numero){
     return $numero % 2 == 0 ? true : false;
 }
-function ordenaArray($arr){
+function ordenaPar($arr){
     $auxi = 0;
     $ordened = true;
     for($i = 0; $i < count($arr) -1; $i++){
@@ -32,9 +27,28 @@ function ordenaArray($arr){
     if($ordened){
         return $arr;
     } else {
-        return ordenaArray($arr);
+        return ordenaPar($arr);
     }
 }
+function ordenaImpar($arr){
+    $auxi = 0;
+    $ordened = true;
+    for($i = 0; $i < count($arr) -1; $i++){
+        if($arr[$i] < $arr[$i + 1]){
+            $auxi = $arr[$i];
+            $arr[$i] = $arr[$i + 1];
+            $arr[$i + 1] = $auxi; 
+            $ordened = false;
+        }
+    }
+    if($ordened){
+        return $arr;
+    } else {
+        return ordenaImpar($arr);
+    }
+}
+
+// Divide o array entre pares e impares
 for($i = 0; $i < count($array); $i++){
     if(ehPar($array[$i])){
         $arrayPar[] =  $array[$i];
@@ -42,16 +56,10 @@ for($i = 0; $i < count($array); $i++){
         $arrayImpar[] =  $array[$i];
     }
 }
-print_r(ordenaArray($arrayImpar));
-print_r(ordenaArray($arrayPar));
-// foreach ($array as $key => $item) {
-//     if ($key != count($array) - 1) {
-//         if ($key != 0) {
-//             $array[$key - 1] = $item;
-//         }
-//     } else {
-//         $array[$key] = $aux;
-//     }
-// }
-// print_r($arrayPar);
-// print_r($arrayImpar);
+
+$ordenedArray = ordenaPar($arrayPar);
+$arrayImpar = ordenaImpar($arrayImpar);
+foreach($arrayImpar as $value){
+    $ordenedArray[] = $value;
+}
+print_r($ordenedArray);
