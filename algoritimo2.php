18879ca2b0afb60fdb801f83ae749dcacbe22841
Lucas  Teles <lucas.mkt@alterdata.com.br>
2021-06-03 20:02:41 -0300

    Algoritimo2 melhorado

--- algoritimo2.php
+++ algoritimo2.php
@@ -3,63 +3,63 @@ if (!defined("STDIN")) {
     define("STDIN", fopen('php://stdin', 'r'));
 }
 
-echo "Digite insira o array a ser ordenado:\n";
+echo "Digite insira o array a ser ordenado: (separado por virgulas)\n";
 $input_array = trim(fgets(STDIN, 1024));
-$array = str_split($input_array);
-$aux = $array[0];
+$array = explode(',', $input_array);
 $arrayPar = [];
 $arrayImpar = [];
 
-function ehPar($numero){
-    return $numero % 2 == 0 ? true : false;
+function ehPar($numero)
+{
+    return $numero % 2 == 0;
 }
-function ordenaPar($arr){
-    $auxi = 0;
+function ordenaCrescente($arr)
+{
+    $aux = 0;
     $ordened = true;
-    for($i = 0; $i < count($arr) -1; $i++){
-        if($arr[$i] > $arr[$i + 1]){
-            $auxi = $arr[$i];
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        if ($arr[$i] > $arr[$i + 1]) {
+            $aux = $arr[$i];
             $arr[$i] = $arr[$i + 1];
-            $arr[$i + 1] = $auxi; 
+            $arr[$i + 1] = $aux;
             $ordened = false;
         }
     }
-    if($ordened){
+    if ($ordened) {
         return $arr;
-    } else {
-        return ordenaPar($arr);
     }
+    return ordenaCrescente($arr);
 }
-function ordenaImpar($arr){
-    $auxi = 0;
+function ordenaDecrescente($arr)
+{
+    $aux = 0;
     $ordened = true;
-    for($i = 0; $i < count($arr) -1; $i++){
-        if($arr[$i] < $arr[$i + 1]){
-            $auxi = $arr[$i];
+    for ($i = 0; $i < count($arr) - 1; $i++) {
+        if ($arr[$i] < $arr[$i + 1]) {
+            $aux = $arr[$i];
             $arr[$i] = $arr[$i + 1];
-            $arr[$i + 1] = $auxi; 
+            $arr[$i + 1] = $aux;
             $ordened = false;
         }
     }
-    if($ordened){
+    if ($ordened) {
         return $arr;
-    } else {
-        return ordenaImpar($arr);
     }
+    return ordenaDecrescente($arr);
 }
 
 // Divide o array entre pares e impares
-for($i = 0; $i < count($array); $i++){
-    if(ehPar($array[$i])){
+for ($i = 0; $i < count($array); $i++) {
+    if (ehPar($array[$i])) {
         $arrayPar[] =  $array[$i];
     } else {
         $arrayImpar[] =  $array[$i];
     }
 }
 
-$ordenedArray = ordenaPar($arrayPar);
-$arrayImpar = ordenaImpar($arrayImpar);
-foreach($arrayImpar as $value){
+$ordenedArray = ordenaCrescente($arrayPar);
+$arrayImpar = ordenaDecrescente($arrayImpar);
+foreach ($arrayImpar as $value) {
     $ordenedArray[] = $value;
 }
 print_r($ordenedArray);
