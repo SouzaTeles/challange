9da0ac86899bd1519b43983e1e175349a85392d4
Lucas  Teles <lucas.mkt@alterdata.com.br>
2021-06-03 19:48:29 -0300

    Ajustes no algoritimo 1

--- algoritimo1.php
+++ algoritimo1.php
@@ -5,16 +5,24 @@ if (!defined("STDIN")) {
 
 echo "Digite insira o array a ser rotacionado:\n";
 $input_array = trim(fgets(STDIN, 1024));
+echo "Digite o numero de rotações:\n";
+$input_times = trim(fgets(STDIN, 8));
 
-$array = str_split($input_array);
-$aux = $array[0];
-foreach ($array as $key => $item) {
-    if ($key != count($array) - 1) {
-        if ($key != 0) {
-            $array[$key - 1] = $item;
+$array = explode(',', $input_array);
+function rotate($arr, $times)
+{
+    $aux = $arr[0];
+    foreach ($arr as $key => $item) {
+        if ($key < count($arr) - 1) {
+                $arr[$key] = $arr[$key + 1];
+        } else {
+            $arr[$key] = $aux;
         }
-    } else {
-        $array[$key] = $aux;
     }
+    $times--;
+    if ($times > 0) {
+        return rotate($arr, $times);
+    }
+    return $arr;
 }
-print_r($array);
+print_r(rotate($array, $input_times));
